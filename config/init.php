1b99b6eb8ba8621f2b6523fe9fc0963cd076bc81
CofChips <qiwei.mod@gmail.com>
2020-11-09 07:52:48 -0800

    added initial structure and bootstrap

---
+++ config/init.php
@@ -0,0 +1,12 @@
+<?php
+// Config file
+require_once 'config.php';
+
+// Autoloader
+
+//require_once('lib/template.php');
+
+function __autoload($class_name){
+    require_once 'lib/'.$class_name. '.php';
+}
+
---
+++ templates/inc/footer.php
@@ -0,0 +1,6 @@
+
+<footer class="container">
+  <p>&copy; CNL 2020</p>
+</footer>
+</body>
+</html>
